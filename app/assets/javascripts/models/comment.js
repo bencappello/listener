a436e20cb10c881ae8a2243d25a547f8c9bb43e1
Ben Cappello <bencappello@gmail.com>
2015-01-26 17:38:43 -0500

    adds timestamp to comment show

--- app/assets/javascripts/models/comment.js
+++ app/assets/javascripts/models/comment.js
@@ -9,4 +9,25 @@ Listener.Models.Comment = Backbone.Model.extend ({
 
     return json;
   },
+
+  postTime: function () {
+    var postDate = new Date(this.get('created_at'));
+    var diff = Date.now() - Date.parse(postDate);
+    var time;
+    var sfx;
+    if ( diff < 3600000) {
+      time = 'less than an hr ago';
+    } else if (diff < 86457091) {
+      time = Math.floor((diff / (60000 * 60)).toString())
+      sfx = (time == 1) ? ' hr ago' : ' hrs ago';
+      time = time.toString() + sfx;
+    } else if (diff < 604918332 ) {
+      time = Math.floor((diff / (60000 * 60 * 24)));
+      sfx = (time == 1) ? ' hr ago' : ' hrs ago';
+      time = time.toString() + sfx;
+    } else {
+      time = 'on ' + postDate.toLocaleDateString();
+    }
+    return time;
+  },
 });
