811f514f2060be33dc91c6730713e5523f93c6fd
Ben Cappello <bencappello@gmail.com>
2015-04-23 15:15:26 -0400

    add eager

--- app/assets/javascripts/listener.js
+++ app/assets/javascripts/listener.js
@@ -12,10 +12,6 @@ window.Listener = {
     this.tags = new Listener.Collections.Tags();
     this.tags.fetch();
 
-    this.allBands = new Listener.Collections.Bands();
-
-    this.allSongs = new Listener.Collections.Songs();
-
     this.topHeader = new Listener.Views.TopHeader({ el: "#top-header" });
     this.audioPlayer = new Listener.Views.AudioPlayer({ el: '#audio-container'})
     this.userHeader = new Listener.Views.UserHeader({ el: "#user-nav" });
